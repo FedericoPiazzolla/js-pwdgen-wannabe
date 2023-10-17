9b97fd9088eaba97d0eebb143e892e20c70e22dc
FedericoPiazzolla <Federicop257@gmail.com>
2023-10-17 17:09:32 +0200

    add: end logic of program

--- js/script.js
+++ js/script.js
@@ -4,5 +4,11 @@ console.log(firstname);
 const lastname = prompt("Inserisci il tuo cognome");
 console.log(lastname)
 const favcolors = prompt("Inserisci il tuo colore preferito");
-console.log(favcolors):
+console.log(favcolors);
 
+// Logica del programma
+const password = `Ti consiglio come password sicura: ${firstname}${lastname}${favcolors}21`;
+console.log(password);
+
+// Output
+document.getElementById("result").innerHTML = password;
