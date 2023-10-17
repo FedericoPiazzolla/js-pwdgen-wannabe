4b1c2da0abc34cf91790418f354aaae9847a2257
FedericoPiazzolla <Federicop257@gmail.com>
2023-10-17 17:02:23 +0200

    add: add variabel at js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Raccolta dei dati
+const firstname = prompt("Inserisci il tuo nome");
+console.log(firstname);
+const lastname = prompt("Inserisci il tuo cognome");
+console.log(lastname)
+const favcolors = prompt("Inserisci il tuo colore preferito");
+console.log(favcolors):
+
